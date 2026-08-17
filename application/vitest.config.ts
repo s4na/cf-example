08ebbfdea131af19ff9dee8838b6ea164a5a4336
Satoshi Nabetani <appletea.umauma@gmail.com>
2026-08-17 14:53:30 +0900

    rebase: restore application/vitest.config.ts

--- application/vitest.config.ts
+++ application/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       reporter: ["text", "json-summary"],
     },
     environment: "jsdom",
-    exclude: ["tests/worker/**", "node_modules/**", "dist/**"],
+    exclude: ["tests/e2e/**", "tests/worker/**", "node_modules/**", "dist/**"],
     setupFiles: ["./tests/setup.ts"],
   },
 });
